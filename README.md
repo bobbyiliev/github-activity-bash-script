a2185d391869481744cf9c046a461d740028463f
Bobby Iliev <bobby@bobbyiliev.com>
2021-01-25 10:06:32 +0000

    Added LICENSE file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# github-activity-bash-script
+# GitHub Activity Demo Script
+
+> Use for demo purposes only!
+
+To execute just run the following commaind inside a demo repository:
+
+```
+wget https://raw.githubusercontent.com/bobbyiliev/github-activity-bash-script/main/activity.sh
+bash activity.sh
+```
+
+Finllly push your changes to GitHub:
+
+```
+git push origin -f your_branch_name"
+```
--- activity.sh
+++ activity.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
+####
+## Use for demo purposes only!
+## To execute just run the following commaind inside a demo repository:
+#
+# wget https://raw.githubusercontent.com/bobbyiliev/github-activity-bash-script/main/activity.sh
+# bash activity.sh
+# # Finllly push your changes to GitHub:
+# git push origin -f your_branch_name"
+####
+
 ##
-# Create a temp commits direcotry
+# Create temp commits direcotry
 ##
 if [[ ! -d .commits ]] ; then
     mkdir -p .commits
+fi
+##
+# Add changes file log
+##
+if [[ ! -f  .commits/changes ]] ; then
+    touch .commits/changes
+    git add .
 fi
 
 ##
 # Create commits for the past 365 days
 ##
 for day in {1..365} ; do
+    # Get the past date of the commit
     day=$(date --date="-${day} day")
     echo "Creating commits for ${day}"
+    # Generate random number of commits for that date
     commits=$(( ( RANDOM % 6 )  + 2 ))
+    # Create the comits
     echo "Creating ${commits} commits"
     for ((i=1;i<=${commits};i++)); do
         content=$(date -d "$day" +"%s")
-        mkdir -p .commits/${content}
-        echo ${content}-${i} >> .commits/${content}/${i}.log
-        git add .
-        git commit -m "Commit number ${content}-${i}"
+        echo ${content}-${i} >> .commits/changes
+        git commit -am "Commit number ${content}-${i}"
         git commit --amend --no-edit --date "${day}"
     done
 done
